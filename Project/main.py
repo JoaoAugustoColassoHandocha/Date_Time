1880ad191a5ff281bbb219318c6b08e96c44f916
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-12-11 17:29:54 -0300

    Update main.py

--- Project/main.py
+++ Project/main.py
@@ -4,3 +4,17 @@
 
 '''
 
+import os, time
+from datetime import datetime
+
+
+while True:
+
+    dia_hora = datetime.now()
+    dia_formatada = dia_hora.strftime('%d/%m/%Y')
+    hora_formatada = dia_hora.strftime('%H:%M:%S')
+    print(f'\n{dia_formatada} | {hora_formatada}')
+        
+    time.sleep(1)
+    
+    os.system('cls' if os.name== 'nt' else 'clear')
